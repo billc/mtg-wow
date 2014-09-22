913b6b7e857704af467a7d008521c7203140db1c
Bill Christian <bill.christian@gmail.com>
2014-09-22 16:01:40 -0400

    Created Wallpaper class to contain name and source location Introduced logic to ignore existing wallpapers in target destination folder.

--- bin/wow.dart
+++ bin/wow.dart
@@ -1,38 +1,57 @@
+library wallpaperoftheweek;
+
 import 'dart:io';
 import 'dart:async';
 
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
 
-main() {
-  var url = "http://magic.wizards.com/en/articles/wallpapers";
+part 'wallpaper.dart';
 
-  String targetLocation = "/Users/billc/Dropbox/Images/magic_wallpaper/";
-  http.read(url).then(selectWallpaper)
-//    .then((Set<String> wallpapers) => removeExisting(wallpapers, targetLocation))
-  .then((loc) => Future.forEach(loc, retrieveImage));
+main() {
+  final String url = "http://magic.wizards.com/en/articles/wallpapers";
+  final String destination = "/Users/billc/Dropbox/Images/magic_wallpaper/";
+
+  http.read(url).then(selectWallpapers).then((wallpapers) => prune(wallpapers, destination)).then((wallpapers) => Future.forEach(wallpapers, process));
 }
 
-Set<String> selectWallpaper(String response) {
+Set<String> selectWallpapers(String response) {
   RegExp exp = new RegExp(r"http[\S]+files\/images\/wallpaper\/[\S]*2560[\S]+.jpg");
   Iterable<Match> matches = exp.allMatches(response);
 
   Set images = new Set();
-
   for (Match m in matches) {
     images.add(m.group(0));
   }
 
-  return images;
+  Set wallpapers = new Set();
+  for (String s in images) {
+    wallpapers.add(new Wallpaper(s, path.basename(s)));
+  }
+
+  return wallpapers;
+}
+
+Set prune(Set wallpapers, String destination) {
+  Set pruned = new Set();
+
+  for (Wallpaper wp in wallpapers) {
+    var f = new File("/Users/billc/Dropbox/Images/magic_wallpaper/${wp.name}");
+    if (!f.existsSync()) {
+      print("New wallpaper found: ${wp.name}");
+      pruned.add(wp);
+    }
+  }
+
+  print("Found ${pruned.length} new wallpapers");
+  return pruned;
 }
 
 // Retrieve the wallpaper from the URL and save to the local target location
 
-Future retrieveImage(String location) {
-  String name = path.basename(location);
-
-  print("Retrieving $location");
-  return http.readBytes(location).then((image) => saveImage(name, image));
+Future process(Wallpaper wp) {
+  print("Retrieving ${wp.source}");
+  return http.readBytes(wp.source).then((image) => saveImage(wp.name, image));
 
 }
 
